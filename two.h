dd6b09d47fee5647c9e3a1ed7382b56a15695162
shehabrakib <110972844+shehabrakib@users.noreply.github.com>
2022-10-25 23:14:18 +0600

    "second commit"

---
+++ two.h
@@ -0,0 +1,20 @@
+#ifndef TWO_H
+#define TWO_H
+// #include "one.h"
+
+class two 
+{
+    public:
+        int x;
+        two();
+        void menuTwo();
+        void create();
+        void read();
+        void update();
+        void delet();
+    protected:
+
+    private:
+};
+
+#endif // TWO_H
